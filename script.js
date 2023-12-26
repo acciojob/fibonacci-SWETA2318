8793962a56e3f267e1ff5d24caa525fc1b2945c3
SWETA2318 <150820032+SWETA2318@users.noreply.github.com>
2023-12-26 19:38:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function fibonacci(num) {
-// your code here
+  if (num === 0) return 0;
+  if (num === 1) return 1;
+
+  let a = 0;
+  let b = 1;
+  let temp;
+
+  for (let i = 2; i <= num; i++) {
+    temp = a + b;
+    a = b;
+    b = temp;
+  }
+
+  return b;
 }
 
 module.exports = fibonacci;
